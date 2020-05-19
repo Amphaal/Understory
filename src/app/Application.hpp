d8b470777890b4bda08cd4b9b0d47601d4c28ae0
Amphaal <guillaume.vara@gmail.com>
2020-05-20 01:01:48 +0200

    wip

--- src/app/Application.hpp
+++ src/app/Application.hpp
@@ -19,27 +19,65 @@
 
 #pragma once
 
+#include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Shaders/VertexColor.h>
 #include <Magnum/Platform/GlfwApplication.h>
+
+#include <utility>
 
 #include "src/base/understory.h"
 
 #include "Utility.hpp"
 
+using namespace Magnum;
+
 namespace UnderStory {
 
-class Application: public Magnum::Platform::Application {
+class Application: public Platform::Application {
  public:
-    explicit Application(const Arguments& arguments): Magnum::Platform::Application{arguments} {
+    explicit Application(const Arguments& arguments): Platform::Application{arguments} {
         this->setWindowTitle(APP_FULL_DENOM);
         this->defineIcon(Utility::getIcon());
+
+        // GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+        // GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+        GL::Renderer::enable(GL::Renderer::Feature::Blending);
+
+        glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+        using namespace Math::Literals;
+
+        struct TriangleVertex {
+            Vector2 position;
+            Color3 color;
+        };
+        const TriangleVertex data[]{
+            {{-0.5f, -0.5f}, 0xff0000_rgbf},    /* Left vertex, red color */
+            {{ 0.5f, -0.5f}, 0x00ff00_rgbf},    /* Right vertex, green color */
+            {{ 0.0f,  0.5f}, 0x0000ff_rgbf}     /* Top vertex, blue color */
+        };
+
+        GL::Buffer buffer;
+        buffer.setData(data);
+
+        _mesh.setCount(3)
+            .addVertexBuffer(std::move(buffer), 0,
+                Shaders::VertexColor2D::Position{},
+                Shaders::VertexColor2D::Color3{});
     }
 
  private:
-    void drawEvent() override {
-        Magnum::GL::defaultFramebuffer.clear(Magnum::GL::FramebufferClear::Color);
+    GL::Mesh _mesh;
+    Shaders::VertexColor2D _shader;
 
-        /* TODO: Add your drawing code here */
+    void drawEvent() override {
+        // GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+
+        _mesh.draw(_shader);
 
         swapBuffers();
     }
