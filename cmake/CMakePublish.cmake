55972d9c153d6cf172a438bce89b13fae3f40821
Guillaume VARA <guillaume.vara@gmail.com>
2020-06-03 15:46:33 +0200

    qqq

--- cmake/CMakePublish.cmake
+++ cmake/CMakePublish.cmake
@@ -32,7 +32,7 @@ SET(CPACK_IFW_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/install.
 INCLUDE(CPack)
 
 #configure default component
-cpack_add_component(${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME})
+cpack_add_component(app)
 
 ######################################
 # CPACK IFW COMPONENTS CONFIGURATION #
@@ -42,7 +42,7 @@ cpack_add_component(${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME})
 INCLUDE(CPackIFW)
 
 # #installer configuration
-cpack_ifw_configure_component(${CMAKE_INSTALL_DEFAULT_COMPONENT_NAME}
+cpack_ifw_configure_component(app
 # FORCED_INSTALLATION
 SCRIPT          "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/ifw/install.js"
 USER_INTERFACES "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/ifw/install.ui"
