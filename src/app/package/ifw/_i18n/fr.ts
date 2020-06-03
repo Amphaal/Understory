71ca6679e770e212c8b43c91947cf8b1ac868b9a
Guillaume VARA <guillaume.vara@gmail.com>
2020-06-03 12:02:00 +0200

    wip

---
+++ src/app/package/ifw/_i18n/fr.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>EndInstallerForm</name>
+    <message>
+        <location filename="install.ui" line="29"/>
+        <source>Launch at close</source>
+        <translation>Lancer à la fermeture</translation>
+    </message>
+</context>
+</TS>
