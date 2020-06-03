3b3f09b75389c99661658ebce0926f95093b0df7
Guillaume VARA <guillaume.vara@gmail.com>
2020-06-03 11:23:38 +0200

    wip

--- cmake/CMakePublish.cmake
+++ cmake/CMakePublish.cmake
@@ -11,26 +11,60 @@ endif()
 set(SETUP_NAME          "${PROJECT_NAME}-setup")
 set(SETUP_NAME_WITH_EXT "${SETUP_NAME}${CPACK_PACKAGE_FILE_EXTENSION}")
 
+# trad
+SET(APP_DESCRIPTION ${PROJECT_DESCRIPTION}
+    fr "L'experience Papier-Crayon intuitive !"
+)
+
 #########
 # CPack #
 #########
 
-set(CPACK_GENERATOR "NSIS")
+set(CPACK_GENERATOR "IFW")
 
+set(CPACK_PACKAGE_DIRECTORY           "CPack")
 set(CPACK_PACKAGE_VENDOR              "LVWL")
 set(CPACK_PACKAGE_NAME                "${PROJECT_NAME}")
 set(CPACK_RESOURCE_FILE_LICENSE       "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README        "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 set(CPACK_PACKAGE_FILE_NAME           "${SETUP_NAME}")
-set(CPACK_PACKAGE_INSTALL_DIRECTORY   "${CPACK_PACKAGE_VENDOR}\\\\${CPACK_PACKAGE_NAME}")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY   "${CPACK_PACKAGE_VENDOR}/${CPACK_PACKAGE_NAME}")
 set(CPACK_PACKAGE_EXECUTABLES         "understory;UnderStory")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_NAME} ${PROJECT_VERSION}")
 
-set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL  ON)
-set(CPACK_NSIS_PACKAGE_NAME                    "${CPACK_PACKAGE_NAME}")
-set(CPACK_NSIS_MUI_ICON                        "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/install.ico")
-set(CPACK_NSIS_UNINSTALL_NAME                  "Uninstall ${CPACK_PACKAGE_NAME}")
+#################
+# IFW specifics #
+#################
+
+# icons
+SET(CPACK_IFW_PACKAGE_LOGO "${CMAKE_CURRENT_SOURCE_DIR}/src/app/resources/logo_64.png")
+SET(CPACK_IFW_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/install.ico")
+
+SET(CPACK_IFW_PACKAGE_WIZARD_STYLE "Modern")
+SET(CPACK_IFW_PACKAGE_START_MENU_DIRECTORY ${CPACK_PACKAGE_INSTALL_DIRECTORY})
+
+#########
+# Setup #
+#########
 
 include(CPack)
+
+# configure default component
+cpack_add_component(app REQUIRED
+    DISPLAY_NAME ${PROJECT_NAME}
+)
+
+INCLUDE(CPackIFW)
+
+# installer configuration
+cpack_ifw_configure_component(app
+    DESCRIPTION ${APP_DESCRIPTION}
+)
+
+# repository for updates
+cpack_ifw_add_repository(coreRepo 
+    URL "https://dl.bintray.com/amphaal/understory/repository"
+)
 
 ###########
 # Zipping #
@@ -39,16 +73,22 @@ include(CPack)
 #create target to be invoked with bash
 add_custom_target(zipForDeploy DEPENDS package)
 
+SET(APP_PACKAGE_DIRECTORY ${CMAKE_BINARY_DIR}/${CPACK_PACKAGE_DIRECTORY})
+SET(APP_REPOSITORY_DIRECTORY ${APP_PACKAGE_DIRECTORY}/_CPack_Packages/${CPACK_SYSTEM_NAME}/IFW/${SETUP_NAME}/repository)
+
 #installer
 add_custom_command(TARGET zipForDeploy
-    COMMAND ${CMAKE_COMMAND} -E tar c installer.zip  --format=zip ${SETUP_NAME_WITH_EXT}
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/installer.zip --format=zip 
+        ${SETUP_NAME_WITH_EXT}
+    WORKING_DIRECTORY ${APP_PACKAGE_DIRECTORY}
     COMMENT "Ziping installer..."
 )
 
 #repository
 add_custom_command(TARGET zipForDeploy
-    COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/repository.zip --format=zip .
-    WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+    COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/repository.zip --format=zip
+        Updates.xml
+        app
+    WORKING_DIRECTORY ${APP_REPOSITORY_DIRECTORY}
     COMMENT "Ziping repository..."
 )
