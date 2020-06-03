67ba67c093722ff07faddfdca5dc05f91e341d27
Guillaume VARA <guillaume.vara@gmail.com>
2020-06-03 15:58:36 +0200

    qqqq

--- cmake/CMakePublish.cmake
+++ cmake/CMakePublish.cmake
@@ -1,158 +1,100 @@
-##########################
-### CPACK CONFIGURATION ##
-##########################
+###########
+# Prepare #
+###########
 
-SET(CPACK_GENERATOR IFW)
+set(SETUP_NAME          "${PROJECT_NAME}-setup")
+set(SETUP_NAME_WITH_EXT "${SETUP_NAME}${CPACK_PACKAGE_FILE_EXTENSION}")
 
-# SET(APP_DESCRIPTION ${PROJECT_NAME}
-#     fr "L'experience JdR simplifiée."
-# )
+# trad
+SET(APP_DESCRIPTION ${PROJECT_DESCRIPTION}
+    fr "L'experience Papier-Crayon intuitive !"
+)
 
-# SET(CPACK_IFW_PACKAGE_WIZARD_STYLE "Modern")
-# SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY ${PROJECT_NAME})
-# SET(CPACK_IFW_PACKAGE_PUBLISHER ${APP_PUBLISHER})
-# SET(CPACK_IFW_PACKAGE_START_MENU_DIRECTORY ${APP_PUBLISHER})
-# SET(CPACK_IFW_PRODUCT_URL ${APP_PATCHNOTE_URL})
-# SET(CPACK_IFW_TARGET_DIRECTORY "@ApplicationsDirX64@/${PROJECT_NAME}")
+#########
+# CPack #
+#########
 
-# IF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-#     SET(CPACK_IFW_PACKAGE_FILE_EXTENSION ".exe")
-# elseif(APPLE)
-#     SET(CPACK_IFW_PACKAGE_FILE_EXTENSION ".dmg")
-# endif()
+set(CPACK_GENERATOR "IFW")
 
-#icons
-# SET(CPACK_IFW_PACKAGE_LOGO "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/logo_64.png")
-# SET(CPACK_IFW_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/install.ico")
+set(CPACK_PACKAGE_DIRECTORY           "CPack")
+set(CPACK_PACKAGE_VENDOR              "LVWL")
+set(CPACK_PACKAGE_NAME                "${PROJECT_NAME}")
+set(CPACK_RESOURCE_FILE_LICENSE       "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README        "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+set(CPACK_PACKAGE_FILE_NAME           "${SETUP_NAME}")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY   "${CPACK_PACKAGE_VENDOR}/${CPACK_PACKAGE_NAME}")
+set(CPACK_PACKAGE_EXECUTABLES         "understory;UnderStory")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_NAME}")
 
-##############
-### INSTALL ##
-##############
+#################
+# IFW specifics #
+#################
 
-INCLUDE(CPack)
+# out ext (must be defined for XCompilation)
+IF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    SET(CPACK_IFW_PACKAGE_FILE_EXTENSION ".exe")
+elseif(APPLE)
+    SET(CPACK_IFW_PACKAGE_FILE_EXTENSION ".dmg")
+endif()
 
-#configure default component
+SET(APP_PACKAGE_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/app/package)
+
+# icons
+SET(CPACK_IFW_PACKAGE_LOGO "${APP_PACKAGE_SOURCES}/logo_64.png")
+SET(CPACK_IFW_PACKAGE_ICON "${APP_PACKAGE_SOURCES}/install.ico")
+
+SET(CPACK_IFW_PACKAGE_WIZARD_STYLE "Modern")
+SET(CPACK_IFW_PACKAGE_START_MENU_DIRECTORY ${CPACK_PACKAGE_INSTALL_DIRECTORY})
+
+#########
+# Setup #
+#########
+
+include(CPack)
+
+# configure default component
 cpack_add_component(app REQUIRED
     DISPLAY_NAME ${PROJECT_NAME}
 )
 
-######################################
-# CPACK IFW COMPONENTS CONFIGURATION #
-######################################
-
-#SET(CPACK_IFW_VERBOSE ON)
 INCLUDE(CPackIFW)
 
-# #installer configuration
+# installer configuration
 cpack_ifw_configure_component(app
-# FORCED_INSTALLATION
-# SCRIPT          "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/ifw/install.js"
-# USER_INTERFACES "${CMAKE_CURRENT_SOURCE_DIR}/src/app/package/ifw/install.ui"
-#TRANSLATIONS "${CMAKE_BINARY_DIR}/fr.qm"
-# DESCRIPTION ${APP_DESCRIPTION}
+    DESCRIPTION      ${APP_DESCRIPTION}
+    SCRIPT          "${APP_PACKAGE_SOURCES}/ifw/install.js"
+    USER_INTERFACES "${APP_PACKAGE_SOURCES}/ifw/install.ui"
+    TRANSLATIONS    "${APP_PACKAGE_SOURCES}/ifw/_i18n/fr.qm"
 )
 
-#repository for updates
+# repository for updates
 cpack_ifw_add_repository(coreRepo 
-URL "https://dl.bintray.com/amphaal/rpgrpz/ifw-${CPACK_SYSTEM_NAME}"
+    URL "https://dl.bintray.com/amphaal/understory/repository"
 )
 
-# ###########
-# # Prepare #
-# ###########
+###########
+# Zipping #
+###########
 
-# if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-#     set(CPACK_PACKAGE_FILE_EXTENSION ".exe")
-# elseif(APPLE)
-#     set(CPACK_PACKAGE_FILE_EXTENSION ".dmg")
-# endif()
+#create target to be invoked with bash
+add_custom_target(zipForDeploy DEPENDS package)
 
-# set(SETUP_NAME          "${PROJECT_NAME}-setup")
-# set(SETUP_NAME_WITH_EXT "${SETUP_NAME}${CPACK_PACKAGE_FILE_EXTENSION}")
+SET(APP_PACKAGE_DIRECTORY ${CMAKE_BINARY_DIR}/${CPACK_PACKAGE_DIRECTORY})
+SET(APP_REPOSITORY_DIRECTORY ${APP_PACKAGE_DIRECTORY}/_CPack_Packages/${CPACK_SYSTEM_NAME}/IFW/${SETUP_NAME}/repository)
 
-# # trad
-# SET(APP_DESCRIPTION ${PROJECT_DESCRIPTION}
-#     fr "L'experience Papier-Crayon intuitive !"
-# )
+#installer
+add_custom_command(TARGET zipForDeploy
+    COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/installer.zip --format=zip 
+        ${SETUP_NAME_WITH_EXT}
+    WORKING_DIRECTORY ${APP_PACKAGE_DIRECTORY}
+    COMMENT "Ziping installer..."
+)
 
-# #########
-# # CPack #
-# #########
-
-# set(CPACK_GENERATOR "IFW")
-
-# # set(CPACK_PACKAGE_DIRECTORY           "CPack")
-# # set(CPACK_PACKAGE_VENDOR              "LVWL")
-# # set(CPACK_PACKAGE_NAME                "${PROJECT_NAME}")
-# # set(CPACK_RESOURCE_FILE_LICENSE       "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
-# # set(CPACK_RESOURCE_FILE_README        "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
-# # set(CPACK_PACKAGE_FILE_NAME           "${SETUP_NAME}")
-# # set(CPACK_PACKAGE_INSTALL_DIRECTORY   "${CPACK_PACKAGE_VENDOR}/${CPACK_PACKAGE_NAME}")
-# # set(CPACK_PACKAGE_EXECUTABLES         "understory;UnderStory")
-# # SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_NAME}")
-
-# #################
-# # IFW specifics #
-# #################
-
-# SET(APP_PACKAGE_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/app/package)
-
-# # icons
-# SET(CPACK_IFW_PACKAGE_LOGO "${APP_PACKAGE_SOURCES}/logo_64.png")
-# SET(CPACK_IFW_PACKAGE_ICON "${APP_PACKAGE_SOURCES}/install.ico")
-
-# SET(CPACK_IFW_PACKAGE_WIZARD_STYLE "Modern")
-# SET(CPACK_IFW_PACKAGE_START_MENU_DIRECTORY ${CPACK_PACKAGE_INSTALL_DIRECTORY})
-
-# #########
-# # Setup #
-# #########
-
-# include(CPack)
-
-# # configure default component
-# cpack_add_component(app REQUIRED
-#     DISPLAY_NAME ${PROJECT_NAME}
-# )
-
-# INCLUDE(CPackIFW)
-
-# # installer configuration
-# cpack_ifw_configure_component(app
-#     DESCRIPTION      ${APP_DESCRIPTION}
-#     SCRIPT          "${APP_PACKAGE_SOURCES}/ifw/install.js"
-#     USER_INTERFACES "${APP_PACKAGE_SOURCES}/ifw/install.ui"
-#     TRANSLATIONS    "${APP_PACKAGE_SOURCES}/ifw/_i18n/fr.qm"
-# )
-
-# # repository for updates
-# cpack_ifw_add_repository(coreRepo 
-#     URL "https://dl.bintray.com/amphaal/understory/repository"
-# )
-
-# ###########
-# # Zipping #
-# ###########
-
-# #create target to be invoked with bash
-# add_custom_target(zipForDeploy DEPENDS package)
-
-# SET(APP_PACKAGE_DIRECTORY ${CMAKE_BINARY_DIR}/${CPACK_PACKAGE_DIRECTORY})
-# SET(APP_REPOSITORY_DIRECTORY ${APP_PACKAGE_DIRECTORY}/_CPack_Packages/${CPACK_SYSTEM_NAME}/IFW/${SETUP_NAME}/repository)
-
-# #installer
-# add_custom_command(TARGET zipForDeploy
-#     COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/installer.zip --format=zip 
-#         ${SETUP_NAME_WITH_EXT}
-#     WORKING_DIRECTORY ${APP_PACKAGE_DIRECTORY}
-#     COMMENT "Ziping installer..."
-# )
-
-# #repository
-# add_custom_command(TARGET zipForDeploy
-#     COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/repository.zip --format=zip
-#         Updates.xml
-#         app
-#     WORKING_DIRECTORY ${APP_REPOSITORY_DIRECTORY}
-#     COMMENT "Ziping repository..."
-# )
+#repository
+add_custom_command(TARGET zipForDeploy
+    COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/repository.zip --format=zip
+        Updates.xml
+        app
+    WORKING_DIRECTORY ${APP_REPOSITORY_DIRECTORY}
+    COMMENT "Ziping repository..."
+)
