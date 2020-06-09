24a731388bc8235904b79490b920ac6c1febdb49
Guillaume VARA <guillaume.vara@gmail.com>
2020-06-09 11:05:12 +0200

    qqq

--- cmake/toolchains/windows-ci.cmake
+++ cmake/toolchains/windows-ci.cmake
@@ -20,31 +20,31 @@ list(APPEND CMAKE_PREFIX_PATH
     ${MINGW64_ROOT}
 )
 
-# #We want Release with debug infos
+#We want Release with debug infos
 SET(CMAKE_BUILD_TYPE Release)
 
-# SET (CMAKE_C_COMPILER                       "clang")
-# SET (CMAKE_CXX_COMPILER                     "clang++")
-# SET (CMAKE_AR                               "llvm-ar")
-# SET (CMAKE_RANLIB                           "llvm-ranlib")
-# SET (CMAKE_NM                               "llvm-nm")
-# SET (CMAKE_RC_COMPILER                      "llvm-rc")
+SET (CMAKE_C_COMPILER                       "clang")
+SET (CMAKE_CXX_COMPILER                     "clang++")
+SET (CMAKE_AR                               "llvm-ar")
+SET (CMAKE_RANLIB                           "llvm-ranlib")
+SET (CMAKE_NM                               "llvm-nm")
+SET (CMAKE_RC_COMPILER                      "llvm-rc")
 
-# SET (CMAKE_C_FLAGS                          "-fuse-ld=lld")
-# SET (CMAKE_CXX_FLAGS                        ${CMAKE_C_FLAGS})
+SET (CMAKE_C_FLAGS                          "-fuse-ld=lld")
+SET (CMAKE_CXX_FLAGS                        ${CMAKE_C_FLAGS})
 
-# SET (CMAKE_C_COMPILER_TARGET                "x86_64-w64-mingw32")
-# SET (CMAKE_CXX_COMPILER_TARGET              ${CMAKE_C_COMPILER_TARGET})
+SET (CMAKE_C_COMPILER_TARGET                "x86_64-w64-mingw32")
+SET (CMAKE_CXX_COMPILER_TARGET              ${CMAKE_C_COMPILER_TARGET})
 
-# SET (GCC_VERSION                            "10.1.0")
+SET (GCC_VERSION                            "10.1.0")
 
-# SET (CMAKE_EXE_LINKER_FLAGS_INIT            "-L ${MINGW64_ROOT}/lib -L ${MINGW64_ROOT}/${CMAKE_C_COMPILER_TARGET}/lib -L ${MINGW64_ROOT}/lib/gcc/${CMAKE_C_COMPILER_TARGET}/${GCC_VERSION}")
-# SET (CMAKE_SHARED_LINKER_FLAGS_INIT         ${CMAKE_EXE_LINKER_FLAGS_INIT})
+SET (CMAKE_EXE_LINKER_FLAGS_INIT            "-L ${MINGW64_ROOT}/lib -L ${MINGW64_ROOT}/${CMAKE_C_COMPILER_TARGET}/lib -L ${MINGW64_ROOT}/lib/gcc/${CMAKE_C_COMPILER_TARGET}/${GCC_VERSION}")
+SET (CMAKE_SHARED_LINKER_FLAGS_INIT         ${CMAKE_EXE_LINKER_FLAGS_INIT})
 
-# SET (CMAKE_C_STANDARD_INCLUDE_DIRECTORIES  
-#     ${MINGW64_ROOT}/include
-#     ${MINGW64_ROOT}/include/c++/${GCC_VERSION}
-#     ${MINGW64_ROOT}/include/c++/${GCC_VERSION}/${CMAKE_C_COMPILER_TARGET}
-#     ${MINGW64_ROOT}/${CMAKE_C_COMPILER_TARGET}/include
-# )
-# SET (CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_C_STANDARD_INCLUDE_DIRECTORIES})
+SET (CMAKE_C_STANDARD_INCLUDE_DIRECTORIES  
+    ${MINGW64_ROOT}/include
+    ${MINGW64_ROOT}/include/c++/${GCC_VERSION}
+    ${MINGW64_ROOT}/include/c++/${GCC_VERSION}/${CMAKE_C_COMPILER_TARGET}
+    ${MINGW64_ROOT}/${CMAKE_C_COMPILER_TARGET}/include
+)
+SET (CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_C_STANDARD_INCLUDE_DIRECTORIES})
