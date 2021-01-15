d99e9c32bbc1e709f0d7a526bd0128cebd4c4d65
Amphaal <guillaume.vara@gmail.com>
2021-01-16 00:56:39 +0100

    bump 0.0.7

--- cmake/CMakePublish.cmake
+++ cmake/CMakePublish.cmake
@@ -6,7 +6,6 @@ SET(APP_PACKAGE_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/package)
 
 set(CPACK_GENERATOR IFW)
 
-set(CPACK_PACKAGE_DIRECTORY           "CPack")
 set(CPACK_PACKAGE_VENDOR              "LVWL")
 set(CPACK_PACKAGE_NAME                ${PROJECT_NAME})
 set(CPACK_RESOURCE_FILE_LICENSE       ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
@@ -26,6 +25,10 @@ elseif(APPLE)
     SET(CPACK_IFW_PACKAGE_FILE_EXTENSION ".dmg")
 endif()
 
+# must be hardset before including CPack, if not, CPACK_PACKAGE_FILE_NAME is overriden for some reason
+SET(SETUP_NAME          ${CPACK_PACKAGE_FILE_NAME})
+SET(SETUP_NAME_WITH_EXT ${CPACK_PACKAGE_FILE_NAME}${CPACK_IFW_PACKAGE_FILE_EXTENSION})
+
 # icons
 SET(CPACK_IFW_PACKAGE_LOGO "${APP_PACKAGE_SOURCES}/resources/logo_64.png")
 SET(CPACK_IFW_PACKAGE_ICON "${APP_PACKAGE_SOURCES}/resources/generated/install.ico")
@@ -44,14 +47,49 @@ INCLUDE(CPackIFW)
 # App
 cpack_add_component("App" DOWNLOADED)
 cpack_ifw_configure_component("App"
-    DISPLAY_NAME ${PROJECT_NAME}
+    DISPLAY_NAME "${PROJECT_NAME} ${PROJECT_VERSION}"
     DESCRIPTION
         ${PROJECT_DESCRIPTION}
         fr "L'experience Papier-Crayon intuitive !"
+    SORTING_PRIORITY 1000
     SCRIPT          "${APP_PACKAGE_SOURCES}/ifw/EndInstallerForm.js"
     USER_INTERFACES "${APP_PACKAGE_SOURCES}/ifw/EndInstallerForm.ui"
     FORCED_INSTALLATION
 )
+
+# Runtime 
+cpack_add_component("Runtime" DOWNLOADED)
+cpack_ifw_configure_component("Runtime"
+    DISPLAY_NAME 
+        "Runtime" 
+        fr "Composants de base"
+    DESCRIPTION 
+        "Essential components used by ${PROJECT_NAME} and other libraries"
+        fr "Composants essentiels utilisés par ${PROJECT_NAME} et autres librairies"
+    SORTING_PRIORITY 900
+    VERSION "1.0.0"
+    FORCED_INSTALLATION
+)
+
+# Magnum
+get_directory_property(MAGNUM_LIBRARY_VERSION
+    DIRECTORY ${CMAKE_SOURCE_DIR}/src/app/magnum 
+    DEFINITION MAGNUM_LIBRARY_VERSION
+)
+cpack_add_component("Magnum" DOWNLOADED)
+cpack_ifw_configure_component("Magnum"
+    DISPLAY_NAME "Magnum ${MAGNUM_LIBRARY_VERSION}"
+    DESCRIPTION 
+        "Essential framework used by ${PROJECT_NAME}"
+        fr "Framework essentiel utilisé par ${PROJECT_NAME}"
+    SORTING_PRIORITY 98
+    VERSION ${MAGNUM_LIBRARY_VERSION}
+    FORCED_INSTALLATION
+)
+
+######################################
+# CPACK IFW COMPONENTS CONFIGURATION #
+######################################
 
 # repository for updates
 cpack_ifw_add_repository(coreRepo 
@@ -62,38 +100,32 @@ cpack_ifw_add_repository(coreRepo
 # Zipping #
 ###########
 
-# source
-SET(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_SYSTEM_NAME}) #override as CPACK_SYSTEM_NAME may end up wrong (CMAKE bug?)
-SET(CPACK_PACKAGE_FILE_NAME_FULL ${CPACK_PACKAGE_FILE_NAME}${CPACK_IFW_PACKAGE_FILE_EXTENSION})
-
-SET(CPACK_PACKAGES_DIR ${CMAKE_BINARY_DIR}/_CPack_Packages)
-SET(APP_REPOSITORY ${CPACK_PACKAGES_DIR}/${CPACK_SYSTEM_NAME}/IFW/${CPACK_PACKAGE_FILE_NAME}/repository)
-
-SET(APP_PACKAGE_LATEST ${CPACK_PACKAGE_NAME}-latest-${CPACK_SYSTEM_NAME})
-SET(APP_PACKAGE_LATEST_FULL ${APP_PACKAGE_LATEST}${CPACK_IFW_PACKAGE_FILE_EXTENSION})
-
 # create target to be invoked with bash
 add_custom_target(zipForDeploy DEPENDS package)
 
-# zip repository
+# variables
+SET(CPACK_PACKAGES_DIR ${CMAKE_BINARY_DIR}/_CPack_Packages)
+SET(APP_REPOSITORY_DIRECTORY ${CPACK_PACKAGES_DIR}/${CPACK_SYSTEM_NAME}/IFW/${SETUP_NAME}/repository)
+
+# repository
 add_custom_command(TARGET zipForDeploy
     COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/repository.zip --format=zip .
-    WORKING_DIRECTORY ${APP_REPOSITORY}
-    COMMENT "Ziping IFW repository..."
+    WORKING_DIRECTORY ${APP_REPOSITORY_DIRECTORY}
+    COMMENT "Ziping repository..."
 )
 
-# zip installer
+#installer
 add_custom_command(TARGET zipForDeploy
-    COMMAND ${CMAKE_COMMAND} -E rename ${CPACK_PACKAGE_FILE_NAME_FULL} ${APP_PACKAGE_LATEST_FULL}
-    COMMAND ${CMAKE_COMMAND} -E tar c installer.zip --format=zip ${APP_PACKAGE_LATEST_FULL}
+    COMMAND ${CMAKE_COMMAND} -E tar c ${CMAKE_BINARY_DIR}/installer.zip --format=zip ${SETUP_NAME_WITH_EXT}
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    COMMENT "Ziping IFW installer..."
+    COMMENT "Ziping installer..."
 )
 
 # cleanup
 add_custom_command(TARGET zipForDeploy
     COMMAND ${CMAKE_COMMAND} -E rm -r
         ${CPACK_PACKAGES_DIR} 
-        ${APP_PACKAGE_LATEST_FULL}
+        ${SETUP_NAME_WITH_EXT}
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     COMMENT "Cleanup CPack files..."
 )
