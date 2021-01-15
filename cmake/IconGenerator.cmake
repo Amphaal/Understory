536d5f8462102825d05a4d183dd30268554fcfd5
Amphaal <guillaume.vara@gmail.com>
2021-01-15 22:48:30 +0100

    fix icon generation

--- cmake/IconGenerator.cmake
+++ cmake/IconGenerator.cmake
@@ -1,3 +1,4 @@
+# determine programs to use and file extensions
 if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
     find_program(convert NAMES convert)
     SET(IconGenerator_SUFFIX ".ico")
@@ -7,23 +8,29 @@ elseif(APPLE)
     SET(IconGenerator_SUFFIX ".icns")
 endif()
 
+# allows to generate icons from PNG file
 function(generateIcon fromPNG toFolder iconName)
-    SET(IconGenerator_OUTPUT_ICON ${CMAKE_CURRENT_SOURCE_DIR}/${toFolder}/${iconName}${IconGenerator_SUFFIX})
-    
+    # override with current source dir
     SET(fromPNG ${CMAKE_CURRENT_SOURCE_DIR}/${fromPNG})
+
+    # define specific output directory target for MacOS
     if(APPLE)
         SET(toFolder ${CMAKE_CURRENT_SOURCE_DIR}/${toFolder}/${iconName}.iconset)
     endif()
     
+    # Generate [${iconName}] icon
     if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-        add_custom_command(OUTPUT ${IconGenerator_OUTPUT_ICON}
-            COMMAND ${convert} -background transparent ${fromPNG} 
-                               -define icon:auto-resize=16,32,48,64,96,128,256 
-                               ${IconGenerator_OUTPUT_ICON}
-            COMMENT "Generate [${iconName}] icon"
+        #
+        SET(IconGenerator_OUTPUT_ICON ${CMAKE_CURRENT_SOURCE_DIR}/${toFolder}/${iconName}${IconGenerator_SUFFIX})
+        
+        # execute
+        execute_process(COMMAND 
+            ${convert} -background transparent ${fromPNG} 
+                       -define icon:auto-resize=16,32,48,64,96,128,256 
+                       ${IconGenerator_OUTPUT_ICON}
         )
     elseif(APPLE)
-        add_custom_command(OUTPUT ${IconGenerator_OUTPUT_ICON}
+        execute_process(
             COMMAND mkdir ${toFolder}
             COMMAND ${sips} -z 16 16     ${fromPNG} --out ${toFolder}/icon_16x16.png
             COMMAND ${sips} -z 32 32     ${fromPNG} --out ${toFolder}/icon_16x16@2x.png
@@ -36,7 +43,6 @@ function(generateIcon fromPNG toFolder iconName)
             COMMAND ${sips} -z 512 512   ${fromPNG} --out ${toFolder}/icon_512x512.png
             COMMAND ${iconutil} -c icns ${toFolder}
             COMMAND rm -R ${toFolder}
-            COMMENT "Generate [${iconName}] icon"
         )
     endif()
 endfunction()
