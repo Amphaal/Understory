fd86f250ad228d058bb3abaa8d45ea06f381d68f
Amphaal <guillaume.vara@gmail.com>
2021-02-04 22:19:37 +0100

    wip including models

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -24,51 +24,49 @@
 // Test cases
 //
 
-// using namespace UnderStory::Network;
+#include "src/core/AssetIntegrator.hpp"
 
-// #include "src/core/AssetIntegrator.hpp"
+TEST_CASE("Test asset integration", "[assets]") {
+    spdlog::set_level(spdlog::level::debug);
 
-// TEST_CASE("Test asset integration", "[assets]") {
-//     spdlog::set_level(spdlog::level::debug);
+    // create package
+    auto path = fs::absolute("./tests/resources/test.png");
+    auto asset = UnderStory::AssetIntegrator::createAsset(path);
+    auto hash = asset.file().file_description().fnv1a_hash();
 
-//     // create package
-//     auto path = fs::absolute("./tests/resources/test.png");
-//     auto asset = UnderStory::AssetIntegrator::createAsset(path);
-//     auto hash = asset.file().file_description().fnv1a_hash();
+    REQUIRE(asset.file().file_description().name() == "test");
+    REQUIRE(asset.file().file_description().file_extension() == ".png");
+    REQUIRE(hash == 9008380550126030887);
+    REQUIRE(asset.file().content_length() == 25579);
+    REQUIRE(asset.dimensions().width() == 290);
+    REQUIRE(asset.dimensions().height() == 300);
 
-//     REQUIRE(asset.file().file_description().name() == "test");
-//     REQUIRE(asset.file().file_description().file_extension() == ".png");
-//     REQUIRE(hash == 9008380550126030887);
-//     REQUIRE(asset.file().content_length() == 25579);
-//     REQUIRE(asset.dimensions().width() == 290);
-//     REQUIRE(asset.dimensions().height() == 300);
+    // integrate package to context
+    auto context = UnderStory::Context::random();
+    UnderStory::AssetContext assetContext(context);
+    assetContext.integrateAsset(asset);
 
-//     // integrate package to context
-//     auto context = UnderStory::Context::random();
-//     UnderStory::AssetContext assetContext(context);
-//     assetContext.integrateAsset(asset);
+    // get package back from context
+    auto assetFromContext = assetContext.fetchAsset(hash);
+    REQUIRE(assetFromContext.file().content_length() == 25579);
+}
 
-//     // get package back from context
-//     auto assetFromContext = assetContext.fetchAsset(hash);
-//     REQUIRE(assetFromContext.file().content_length() == 25579);
-// }
+#include "src/core/UpdateChecker.hpp"
+using UnderStory::UpdateChecker_Private;
 
-// #include "src/core/UpdateChecker.hpp"
-// using UnderStory::UpdateChecker_Private;
+TEST_CASE("Version comparaison", "[update checker]") {
+    spdlog::set_level(spdlog::level::debug);
 
-// TEST_CASE("Version comparaison", "[update checker]") {
-//     spdlog::set_level(spdlog::level::debug);
+    std::string test_localVersion("0.5.0");
+    auto testUpdateChecker = [test_localVersion](const std::string &remoteVersion) {
+        return UpdateChecker_Private::_isRemoteVersionNewerThanLocal(remoteVersion, test_localVersion);
+    };
 
-//     std::string test_localVersion("0.5.0");
-//     auto testUpdateChecker = [test_localVersion](const std::string &remoteVersion) {
-//         return UpdateChecker_Private::_isVersionNewerThanLocal(remoteVersion, test_localVersion);
-//     };
+    REQUIRE(testUpdateChecker("0.5.1"));
+    REQUIRE(testUpdateChecker("0.5.10"));
+    REQUIRE(testUpdateChecker("1.0"));
 
-//     REQUIRE(testUpdateChecker("0.5.1"));
-//     REQUIRE(testUpdateChecker("0.5.10"));
-//     REQUIRE(testUpdateChecker("1.0"));
-
-//     REQUIRE_FALSE(testUpdateChecker(APP_CURRENT_VERSION));
-//     REQUIRE_FALSE(testUpdateChecker("0.5.0"));
-//     REQUIRE_FALSE(testUpdateChecker("0.4.10"));
-// }
+    REQUIRE_FALSE(testUpdateChecker(APP_CURRENT_VERSION));
+    REQUIRE_FALSE(testUpdateChecker("0.5.0"));
+    REQUIRE_FALSE(testUpdateChecker("0.4.10"));
+}
